c259b80b23d7b910a3d433028f5b422493c7e656
Margaret Qian <myq2000@columbia.edu>
2018-04-14 02:26:40 +0000

    conver maps

--- convert_maps.py
+++ convert_maps.py
@@ -4,7 +4,7 @@ from skimage import io
 import json
 
 if __name__ == '__main__':
-    with open('citscapes.json') as data_file:
+    with open('cityscapes.json') as data_file:
         data = json.load(data_file)
 
     color_dict = {}
@@ -12,17 +12,21 @@ if __name__ == '__main__':
     for i, color in enumerate(palette):
         color_dict[tuple(color)] = i
 
-    img_names = os.listdir(./semantics)
+    img_names = os.listdir('./semantics')
 
     for name in img_names:
-        image = io.imread(os.path.join(self.root_dir, img_name))
-        new_img = np.array((image.shape[1], image.shape[2]))
-        for i in range(image.shape[1]):
-            for j in range(image.shape[2]):
-                label = color_dict[tuple(image[:, i, j])]
+        image = io.imread(os.path.join('./semantics', name))
+        new_img = np.empty((image.shape[0], image.shape[1]))
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                color = tuple(image[i, j, :])
+                if color in color_dict:
+                    label = color_dict[tuple(image[i, j, :])]
+                else:
+                    label = -1
                 new_img[i, j] = label
 
-        np.save(new_imgs, './maps/' + name)
+        np.save('./maps/' + name, new_img)
 
 
 
