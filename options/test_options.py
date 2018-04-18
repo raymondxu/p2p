063b38cffe5c94ce1b40dcda6413f616f81df3de
Raymond Xu <raymondxumail@gmail.com>
2018-04-18 04:26:09 +0000

    Add vae test option, need to fix inputs (1) dimensions (2) round to 33 colors (3) change to labels 1-33

--- options/test_options.py
+++ options/test_options.py
@@ -5,6 +5,9 @@ from .base_options import BaseOptions
 class TestOptions(BaseOptions):
     def initialize(self):
         BaseOptions.initialize(self)
+
+        self.parser.add_argument('--vae', type=int, default=0, help='whether the inputs are from our encoder.')
+
         self.parser.add_argument('--ntest', type=int, default=float("inf"), help='# of test examples.')
         self.parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
         self.parser.add_argument('--aspect_ratio', type=float, default=1.0, help='aspect ratio of result images')
@@ -13,3 +16,4 @@ class TestOptions(BaseOptions):
         self.parser.add_argument('--how_many', type=int, default=50, help='how many test images to run')       
         self.parser.add_argument('--cluster_path', type=str, default='features_clustered_010.npy', help='the path for clustered results of encoded features')
         self.isTrain = False
+
