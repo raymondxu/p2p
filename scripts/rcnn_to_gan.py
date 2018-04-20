4d08f9909c7f1514d321380d5951c2ef96b4344a
Raymond Xu <raymondxumail@gmail.com>
2018-04-20 18:44:01 -0400

    Untested conversion script

--- scripts/rcnn_to_gan.py
+++ scripts/rcnn_to_gan.py
@@ -5,6 +5,10 @@ from PIL import Image
 import numpy as np
 
 
+GAN_SEG_MAP_DIR = 'test_label'
+GAN_INST_DIR = 'test_inst'
+
+
 # I inverted https://github.com/facebookresearch/Detectron/blob/master/lib/datasets/cityscapes/coco_to_cityscapes_id.py
 def coco_to_cityscapes_with_rider(coco_id):
     lookup = {
@@ -18,39 +22,72 @@ def coco_to_cityscapes_with_rider(coco_id):
         6: 7,  # bus
         1: 8,  # rider ("person", *rider has human right!*)
     }
-    return lookup[cityscapes_id]
+    return lookup[coco_id]
 
 
-GAN_SEG_MAP_DIR = 'test_label'
-GAN_INST_DIR = 'test_inst'
+def process_all(input_mask_dir, input_class_map_dir, output_dir):
+    for mask, class_map in zip(os.listdir(input_mask_dir), os.listdir(input_class_map_dir)):
+        print('Processing {} and {}'.format(mask, class_map))
+        make_seg(mask, input_mask_dir, class_map, input_class_dir, output_dir)
+        make_inst(mask, input_mask_dir, class_map, input_class_dir, output_dir)
 
 
-def process_all(input_dir, output_dir):
-    for f in os.listdir(input_dir):
-        print('Processing {}'.format(f))
+def make_seg(mask, input_mask_dir, clazz, input_class_dir, output_dir):
+    """
+    Produces a cityscapes segmentation map from an object mask and a class map.
 
-        img = Image.open(os.path.join(input_dir, f))
-        seg_data = np.zeros(img.size)
-        inst_data = np.zeros(img.size)
-        w, h = img.size
+    Identified pixels will have values corresponding to their cityscapes class id.
+    Unidentified pixels will have value 0.
+    """
+    mask = np.fromfile(os.path.join(input_mask_dir, mask), dtype='bool')
+    class_map = np.fromfile(os.path.join(input_class_dir, clazz))
 
-        for i in range(w):
-            for j in range(h):
-                # Do work here, write to the np arrays
-                pass
+    print(mask.shape)
+    print(class_map.shape)
+    objs, w, h = mask.shape
+    seg_data = np.zeros((w, h))
 
-        seg_img = Image.fromarray(seg_data.astype('uint8'), mode='L')
-        seg_img.save(os.path.join(output_dir, GAN_SEG_MAP_DIR, '{}'.format(f)))
-        # seg_img.show()
+    for mask_layer, coco_class_id in enumerate(class_map):
+        seg_data += mask[mask_layer,:,:] * (coco_to_cityscapes_with_rider(coco_class_id) * np.ones((w, h)))
 
-        inst_img = Image.fromarray(inst_data.astype('int32'), mode='I')
-        inst_img.save(os.path.join(output_dir, GAN_INST_MAP_DIR, '{}'.format(f)))
-        # inst_img.show()
+    seg_img = Image.fromarray(seg_data.astype('uint8'), mode='L')
+    seg_img.save(os.path.join(output_dir, GAN_SEG_MAP_DIR, '{}'.format(mask)))
+    # seg_img.show()
+
+
+def make_inst(mask, input_mask_dir, class_map, input_class_map_dir, output_dir):
+    """
+    Produces a cityscapes instance map from an object mask and a class map.
+
+    Each object will be colored a distinct arbitrary color such that the GAN edge processor
+    will be able to draw object boundaries. Non-objects will have value 0.
+    """
+    mask = np.fromfile(os.path.join(input_mask_dir, mask), dtype='bool')
+    class_map = np.fromfile(os.path.join(input_class_map_dir, class_map))
+
+    objs, w, h = mask.shape
+    inst_data = np.zeros((w, h))
+    object_counter = 1
+
+    for mask_layer in range(len(class_map)):
+        inst_data += mask[mask_layer,:,:] * (object_counter * np.ones((w, h)))
+        object_counter += 1
+
+    inst_img = Image.fromarray(inst_data.astype('int32'), mode='I')
+    inst_img.save(os.path.join(output_dir, GAN_INST_MAP_DIR, '{}'.format(mask)))
 
 
 if __name__ == '__main__':
-    input_dir = sys.argv[1]
-    output_dir = sys.argv[2]
+    """
+    Usage: python3 rcnn_to_gan <input_masks_dir> <input_class_map_dir> <output_dir>
+
+    Masks are one-hot npy files of shape (n_classes, w, h).
+    Class maps are npy files of shape (n_classes, 1) and maps each layer of the mask to a coco class.
+    The created files will be written to `<output_dir>/test_label` and `<output_dir>/test_inst`.
+    """
+    input_mask_dir = sys.argv[1]
+    input_class_map_dir = sys.argv[2]
+    output_dir = sys.argv[3]
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
-    process_all(input_dir, output_dir)
+    process_all(input_mask_dir, input_class_map_dir, output_dir)
