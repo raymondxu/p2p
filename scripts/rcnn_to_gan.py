bc3049736718fffee555d88ba955231a113d494e
Raymond Xu <raymondxumail@gmail.com>
2018-04-20 20:52:31 -0400

    Fix data loading, seg map generation works

--- scripts/rcnn_to_gan.py
+++ scripts/rcnn_to_gan.py
@@ -28,53 +28,49 @@ def coco_to_cityscapes_with_rider(coco_id):
 def process_all(input_mask_dir, input_class_map_dir, output_dir):
     for mask, class_map in zip(os.listdir(input_mask_dir), os.listdir(input_class_map_dir)):
         print('Processing {} and {}'.format(mask, class_map))
-        make_seg(mask, input_mask_dir, class_map, input_class_dir, output_dir)
-        make_inst(mask, input_mask_dir, class_map, input_class_dir, output_dir)
+        make_seg(mask, input_mask_dir, class_map, input_class_map_dir, output_dir)
+        make_inst(mask, input_mask_dir, class_map, input_class_map_dir, output_dir)
 
 
-def make_seg(mask, input_mask_dir, clazz, input_class_dir, output_dir):
+def make_seg(mask_name, mask_dir, class_map_name, class_map_dir, output_dir):
     """
     Produces a cityscapes segmentation map from an object mask and a class map.
 
     Identified pixels will have values corresponding to their cityscapes class id.
     Unidentified pixels will have value 0.
     """
-    mask = np.fromfile(os.path.join(input_mask_dir, mask), dtype='bool')
-    class_map = np.fromfile(os.path.join(input_class_dir, clazz))
+    mask = np.load(os.path.join(mask_dir, mask_name))
+    class_map = np.load(os.path.join(class_map_dir, class_map_name))
 
-    print(mask.shape)
-    print(class_map.shape)
-    objs, w, h = mask.shape
+    w, h, objs = mask.shape
     seg_data = np.zeros((w, h))
 
     for mask_layer, coco_class_id in enumerate(class_map):
-        seg_data += mask[mask_layer,:,:] * (coco_to_cityscapes_with_rider(coco_class_id) * np.ones((w, h)))
+        seg_data += mask[:,:,mask_layer] * (coco_to_cityscapes_with_rider(coco_class_id) * np.ones((w, h)))
 
     seg_img = Image.fromarray(seg_data.astype('uint8'), mode='L')
-    seg_img.save(os.path.join(output_dir, GAN_SEG_MAP_DIR, '{}'.format(mask)))
-    # seg_img.show()
+    seg_img.save(os.path.join(output_dir, GAN_SEG_MAP_DIR, '{}.png'.format(mask_name[:-4])))
 
 
-def make_inst(mask, input_mask_dir, class_map, input_class_map_dir, output_dir):
+def make_inst(mask_name, mask_dir, class_map_name, class_map_dir, output_dir):
     """
     Produces a cityscapes instance map from an object mask and a class map.
 
     Each object will be colored a distinct arbitrary color such that the GAN edge processor
     will be able to draw object boundaries. Non-objects will have value 0.
     """
-    mask = np.fromfile(os.path.join(input_mask_dir, mask), dtype='bool')
-    class_map = np.fromfile(os.path.join(input_class_map_dir, class_map))
-
-    objs, w, h = mask.shape
+    mask = np.load(os.path.join(mask_dir, mask_name))
+    class_map = np.load(os.path.join(class_map_dir, class_map_name))
+    
+    w, h, objs = mask.shape
     inst_data = np.zeros((w, h))
-    object_counter = 1
+    object_counter = 100
 
     for mask_layer in range(len(class_map)):
-        inst_data += mask[mask_layer,:,:] * (object_counter * np.ones((w, h)))
-        object_counter += 1
+        inst_data += mask[:,:,mask_layer] * (object_counter * np.ones((w, h)))
 
     inst_img = Image.fromarray(inst_data.astype('int32'), mode='I')
-    inst_img.save(os.path.join(output_dir, GAN_INST_MAP_DIR, '{}'.format(mask)))
+    inst_img.save(os.path.join(output_dir, GAN_INST_DIR, '{}.png'.format(mask_name[:-4])))
 
 
 if __name__ == '__main__':
@@ -88,6 +84,14 @@ if __name__ == '__main__':
     input_mask_dir = sys.argv[1]
     input_class_map_dir = sys.argv[2]
     output_dir = sys.argv[3]
+    
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
+    seg_path = os.path.join(output_dir, GAN_SEG_MAP_DIR)
+    inst_path = os.path.join(output_dir, GAN_INST_DIR)
+    if not os.path.exists(seg_path):
+        os.mkdir(seg_path)
+    if not os.path.exists(inst_path):
+        os.mkdir(inst_path)
+
     process_all(input_mask_dir, input_class_map_dir, output_dir)
