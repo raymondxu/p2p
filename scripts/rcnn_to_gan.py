ebee9a83f838c9111b983aa1d9ea18ccf29a3562
Raymond Xu <raymondxumail@gmail.com>
2018-04-21 11:44:19 -0700

    Fix cityscapes class mapping, but now instance map fails assertions

--- scripts/rcnn_to_gan.py
+++ scripts/rcnn_to_gan.py
@@ -9,18 +9,17 @@ GAN_SEG_MAP_DIR = 'test_label'
 GAN_INST_DIR = 'test_inst'
 
 
-# I inverted https://github.com/facebookresearch/Detectron/blob/master/lib/datasets/cityscapes/coco_to_cityscapes_id.py
-def coco_to_cityscapes_with_rider(coco_id):
+def coco_to_cityscapes_id(coco_id):
     lookup = {
         0: 0,  # ... background
-        2: 1,  # bicycle
-        3: 2,  # car
-        1: 3,  # person
-        7: 4,  # train
-        8: 5,  # truck
-        4: 6,  # motorcycle
-        6: 7,  # bus
-        1: 8,  # rider ("person", *rider has human right!*)
+        1: 33,  # bicycle
+        2: 26,  # car
+        3: 24,  # person
+        4: 31,  # train
+        5: 27,  # truck
+        6: 32,  # motorcycle
+        7: 28,  # bus
+        8: 25,  # rider ("person", *rider has human right!*)
     }
     return lookup[coco_id]
 
@@ -46,7 +45,7 @@ def make_seg(mask_name, mask_dir, class_map_name, class_map_dir, output_dir):
     seg_data = np.zeros((w, h))
 
     for mask_layer, coco_class_id in enumerate(class_map):
-        seg_data += mask[:,:,mask_layer] * (coco_to_cityscapes_with_rider(coco_class_id) * np.ones((w, h)))
+        seg_data += mask[:,:,mask_layer] * (coco_to_cityscapes_id(coco_class_id) * np.ones((w, h)))
 
     seg_img = Image.fromarray(seg_data.astype('uint8'), mode='L')
     seg_img.save(os.path.join(output_dir, GAN_SEG_MAP_DIR, '{}.png'.format(mask_name[:-4])))
@@ -66,8 +65,9 @@ def make_inst(mask_name, mask_dir, class_map_name, class_map_dir, output_dir):
     inst_data = np.zeros((w, h))
     object_counter = 100
 
-    for mask_layer in range(len(class_map)):
-        inst_data += mask[:,:,mask_layer] * (object_counter * np.ones((w, h)))
+    for mask_layer, coco_class_id in enumerate(class_map):
+        if coco_class_id > 0:
+            inst_data += mask[:,:,mask_layer] * (object_counter * np.ones((w, h)))
 
     inst_img = Image.fromarray(inst_data.astype('int32'), mode='I')
     inst_img.save(os.path.join(output_dir, GAN_INST_DIR, '{}.png'.format(mask_name[:-4])))
