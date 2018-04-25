8457946efd415e13c2c3d6ce55f550743c429e82
MargaretQian <myq2000@columbia.edu>
2018-04-25 05:00:34 +0000

    overla script

--- scripts/overlay_imgs.py
+++ scripts/overlay_imgs.py
@@ -1,8 +1,10 @@
 from skimage import io
 import sys
+import numpy as np
+from PIL import Image
 
 def overlay_semantic_map(front, back):
-    overlay = np.where(front != 0, front, back)
+    overlay = np.where(front > 1, front, back)
     return overlay
 
 if __name__ == '__main__':
@@ -10,5 +12,9 @@ if __name__ == '__main__':
     img2 = io.imread(sys.argv[2])
     output = overlay_semantic_map(img1, img2)
 
-    output = Image.fromarray(output)
-    output.save("overlay.png")
+    if sys.argv[3] == "inst":
+        output = Image.fromarray(output.astype('int32'), mode='I')
+        output.save("overlay.png")
+    else:
+        output = Image.fromarray(output)
+        output.save("overlay.png")
