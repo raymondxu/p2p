3cdbaaab31c5048136baa9c2ecd08e8730b32cc8
Raymond Xu <raymondxumail@gmail.com>
2018-04-25 01:24:24 -0400

    Modularize and include overlay

--- master.py
+++ master.py
@@ -8,33 +8,57 @@ import numpy as np
 
 from Mask_RCNN import rcnn_main
 from scripts.rcnn_to_gan import rcnn_to_gan_main
+from scripts.overlay_imgs import overlay_main
 
 
 ROOT_DIR = os.path.abspath('./')
 TEMP_DIR = os.path.join(ROOT_DIR, 'master_temp')
+
 RCNN_OUTPUT_MASK_DIR = os.path.join(TEMP_DIR, 'rcnn_mask')
 RCNN_OUTPUT_CLASS_MAP_DIR = os.path.join(TEMP_DIR, 'rcnn_class_map')
+
+OVERLAY_INPUT_DIR = os.path.join(TEMP_DIR, 'overlay_input')
+OVERLAY_INPUT_SEG_DIR = os.path.join(OVERLAY_INPUT_DIR, 'test_label')
+OVERLAY_INPUT_INST_DIR = os.path.join(OVERLAY_INPUT_DIR, 'test_inst')
+
 GAN_INPUT_DIR = os.path.join(ROOT_DIR, 'datasets/master')
+OVERLAY_OUTPUT_SEG_DIR = os.path.join(GAN_INPUT_DIR, 'test_label')
+OVERLAY_OUTPUT_INST_DIR = os.path.join(GAN_INPUT_DIR, 'test_inst')
 GAN_OUTPUT_DIR = os.path.join(ROOT_DIR, 'results_rcnn/test_1024p')
 
 
-parser = argparse.ArgumentParser(description='Segmentation-Driven Image Variation Generation Pipeline')
-parser.add_argument('-input', default='master_input', help='the path to the input directory containing photo images')
-parser.add_argument('-output', default='master_output', help='the path to the output directory where the image variants will be placed')
-
-
-def process_all(input_dir, output_dir):
+def process_all(input_dir, output_dir, bg_seg, bg_inst):
     for f in os.listdir(input_dir):
         print('Processing {}'.format(f))
+        run_rcnn(input_dir)
+        run_rcnn_to_gan()
+        run_overlay(bg_seg, bg_inst)
+        run_gan(output_dir)
 
 
 def run_rcnn(input_dir):
     print('Running RCNN...')
     rcnn_main(input_dir)
 
+
 def run_rcnn_to_gan():
     print('Running rcnn_to_gan...')
-    rcnn_to_gan_main(RCNN_OUTPUT_MASK_DIR, RCNN_OUTPUT_CLASS_MAP_DIR, GAN_INPUT_DIR)
+    rcnn_to_gan_main(RCNN_OUTPUT_MASK_DIR, RCNN_OUTPUT_CLASS_MAP_DIR, OVERLAY_INPUT_DIR)
+
+
+def run_overlay(bg_seg, bg_inst):
+    print('Running overlay...')
+    # Overlay seg maps
+    overlay_main(OVERLAY_INPUT_SEG_DIR,
+                 OVERLAY_OUTPUT_SEG_DIR,
+                 bg_seg,
+                 seg=True)
+
+    # Overlay inst maps
+    overlay_main(OVERLAY_INPUT_INST_DIR,
+                 OVERLAY_OUTPUT_INST_DIR,
+                 bg_inst,
+                 seg=False)
 
 
 def run_gan(output_dir):
@@ -50,13 +74,21 @@ def run_gan(output_dir):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Segmentation-Driven Image Variation Generation Pipeline')
+    parser.add_argument('-input', default='master_input', help='the path to the input directory containing photo images')
+    parser.add_argument('-output', default='master_output', help='the path to the output directory where the image variants will be placed')
+    parser.add_argument('bg_seg', help='the path to the background semantic map to generate variants in the foreground of')
+    parser.add_argument('bg_inst', help='the path to the background instance map to generate variants in the foreground of')
+
     args = parser.parse_args()
     input_dir = args.input
     output_dir = args.output
+    bg_seg = args.bg_seg
+    bg_inst = args.bg_inst
 
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
     if not os.path.exists(TEMP_DIR):
         os.mkdir(TEMP_DIR)
 
-    process_all(input_dir, output_dir)
+    process_all(input_dir, output_dir, bg_seg, bg_inst)
--- scripts/overlay_imgs.py
+++ scripts/overlay_imgs.py
@@ -3,14 +3,26 @@ import sys
 import numpy as np
 from PIL import Image
 
-def overlay_semantic_map(front, back):
+
+def overlay_map(front, back):
     overlay = np.where(front > 1, front, back)
     return overlay
 
+
+def overlay_main(input_dir, output_dir, bg, seg=True):
+    bg = io.imread(bg)
+    for f in os.listdir(input_dir):
+        fg = io.imread(os.path.join(input_dir, f))
+        out_map = overlay_map(fg, bg)
+        if seg:
+            out_img = Image.fromarray(out_map)
+        else:  # inst
+            out_img = Image.fromarray(out_map.astype('int32'), mode='I')
+        out_img.save(os.path.join(output_dir, f))
+
+
 if __name__ == '__main__':
-    img1 = io.imread(sys.argv[1])
-    img2 = io.imread(sys.argv[2])
-    output = overlay_semantic_map(img1, img2)
+    output = overlay_map(img1, img2)
 
     if sys.argv[3] == "inst":
         output = Image.fromarray(output.astype('int32'), mode='I')
